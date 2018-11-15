821f9217a78801f108c4481cc0574746d746b9f9
shadrachhepner <45045014+shadrachhepner@users.noreply.github.com>
2018-11-15 08:51:37 -0500

    Add files via upload
    Forgot process_fun

---
+++ process_fun.m
@@ -0,0 +1,10 @@
+function process_return = process_fun(a,c,particles,N_tot,P)
+
+    stuff = num2cell(zeros(length(P),1)); %holder for fnc handles
+    process_return = zeros(length(P),1);
+    
+    for i = 1:length(P)
+        stuff{i} = str2func(['@(a,c,particles,N_tot)', P(i).R_str]); %DO BETTER
+        process_return(i) = stuff{i}(a,c,particles,N_tot);
+    end
+end
